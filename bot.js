0b8d71fb2fda788606b2b9212773d27855005763
emberglaze <68788282+NoNameLmao@users.noreply.github.com>
2023-09-19 15:46:14 +0300

    remove duplicate .once('spawn')

--- bot.js
+++ bot.js
@@ -23,67 +23,66 @@ function createBot() {
 		bot.loadPlugin(pathfinder);
 		const defaultMove = new Movements(bot);
 		bot.pathfinder.setMovements(defaultMove);
-		bot.once('spawn', () => {
-			logger.info('Bot joined to the server');
-			if (config.utils['auto-auth'].enabled) {
-				logger.info('Started auto-auth module');
-				let password = config.utils['auto-auth'].password;
-				setTimeout(() => {
-					bot.chat(`/register ${password} ${password}`);
-					bot.chat(`/login ${password}`);
-				}, 500);
-				logger.info(`Authentication commands executed`);
-			}
-			if (config.utils['chat-messages'].enabled) {
-				logger.info('Started chat-messages module');
-				let messages = config.utils['chat-messages'].messages;
-				if (config.utils['chat-messages'].repeat) {
-					let delay = config.utils['chat-messages']['repeat-delay'];
-					let i = 0;
-					setInterval(() => {
-						bot.chat(`${messages[i]}`);
-						if (i + 1 === messages.length) i = 0;
-						else i++;
-					}, delay * 1000);
-				} else messages.forEach(msg => bot.chat(msg));
-			}
-			const pos = config.position;
-			if (config.position.enabled) {
-				logger.info(
-					`Starting moving to target location (${pos.x}, ${pos.y}, ${pos.z})`
-				);
-				bot.pathfinder.setGoal(new GoalBlock(pos.x, pos.y, pos.z));
-			}
-			if (config.utils['anti-afk'].enabled) {
-				if (config.utils['anti-afk'].sneak) bot.setControlState('sneak', true);
-				if (config.utils['anti-afk'].jump) bot.setControlState('jump', true);
-				if (config.utils['anti-afk'].hit.enabled) {
-					let delay = config.utils['anti-afk'].hit.delay;
-					let attackMobs = config.utils['anti-afk'].hit['attack-mobs']
-	
-					setInterval(() => {
-						if (attackMobs) {
-							const excludedTypes = ['object', 'player', 'global', 'orb', 'other'];
-							let entity = bot.nearestEntity(e => !excludedTypes.includes(e.type));
-							if (entity) {
-								bot.attack(entity);
-								return
-							}
+		logger.info('Bot joined to the server');
+		if (config.utils['auto-auth'].enabled) {
+			logger.info('Started auto-auth module');
+			let password = config.utils['auto-auth'].password;
+			setTimeout(() => {
+				bot.chat(`/register ${password} ${password}`);
+				bot.chat(`/login ${password}`);
+			}, 500);
+			logger.info(`Authentication commands executed`);
+		}
+		if (config.utils['chat-messages'].enabled) {
+			logger.info('Started chat-messages module');
+			let messages = config.utils['chat-messages'].messages;
+			if (config.utils['chat-messages'].repeat) {
+				let delay = config.utils['chat-messages']['repeat-delay'];
+				let i = 0;
+				setInterval(() => {
+					bot.chat(`${messages[i]}`);
+					if (i + 1 === messages.length) i = 0;
+					else i++;
+				}, delay * 1000);
+			} else messages.forEach(msg => bot.chat(msg));
+		}
+		const pos = config.position;
+		if (config.position.enabled) {
+			logger.info(
+				`Starting moving to target location (${pos.x}, ${pos.y}, ${pos.z})`
+			);
+			bot.pathfinder.setGoal(new GoalBlock(pos.x, pos.y, pos.z));
+		}
+		if (config.utils['anti-afk'].enabled) {
+			if (config.utils['anti-afk'].sneak) bot.setControlState('sneak', true);
+			if (config.utils['anti-afk'].jump) bot.setControlState('jump', true);
+			if (config.utils['anti-afk'].hit.enabled) {
+				let delay = config.utils['anti-afk'].hit.delay;
+				let attackMobs = config.utils['anti-afk'].hit['attack-mobs']
+
+				setInterval(() => {
+					if (attackMobs) {
+						const excludedTypes = ['object', 'player', 'global', 'orb', 'other'];
+						let entity = bot.nearestEntity(e => !excludedTypes.includes(e.type));
+						if (entity) {
+							bot.attack(entity);
+							return
 						}
-						bot.swingArm('right', true);
-					}, delay);
-				}
-				if (config.utils['anti-afk'].rotate) {
-					setInterval(() => {
-						bot.look(bot.entity.yaw + 1, bot.entity.pitch, true);
-					}, 100);
-				}
-				if (config.utils['anti-afk']['circle-walk'].enabled) {
-					let radius = config.utils['anti-afk']['circle-walk'].radius;
-					circleWalk(bot, radius);
-				}
+					}
+					bot.swingArm('right', true);
+				}, delay);
 			}
-		}).on('death', () => {
+			if (config.utils['anti-afk'].rotate) {
+				setInterval(() => {
+					bot.look(bot.entity.yaw + 1, bot.entity.pitch, true);
+				}, 100);
+			}
+			if (config.utils['anti-afk']['circle-walk'].enabled) {
+				let radius = config.utils['anti-afk']['circle-walk'].radius;
+				circleWalk(bot, radius);
+			}
+		}
+		bot.on('death', () => {
 			logger.warn(`Bot has been died and was respawned at ${bot.entity.position}`);
 		}).on('kicked', reason => {
 			let reasonText = JSON.parse(reason).text;
